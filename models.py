d0d979c897fd4e336c7ce289be1d9935514642e7
codebiscuits <ross_coates82@hotmail.com>
2026-02-13 13:36:54 +0000

    Add Task model with toggle_comple functionality

---
+++ models.py
@@ -0,0 +1,15 @@
+class Task():
+
+    def __init__(self, id: int, description: str):
+        self.id = id
+        self.description = description
+        self.is_complete: bool = False
+
+    def __repr__(self):
+        if self.is_complete:
+            return f"- [x] {self.id} {self.description}"
+        else:
+            return f"- [ ] {self.id} {self.description}"
+
+    def toggle_completed(self):
+        self.is_complete = not self.is_complete
